0d0198be6b51cac783c75e6cc02ce01eb4114c71
Robin9777 <rbouilliol@gmail.com>
2026-02-05 12:09:29 +0100

    tunning pipeline

--- DecisionTreePipeline.py
+++ DecisionTreePipeline.py
@@ -4,12 +4,13 @@ import numpy as np
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer, SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 import utilsfunc as uf
 from config import SELECTED_FEATURES, TARGETS, MAPPING
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.compose import make_column_selector
 import pickle
 
 
@@ -41,31 +42,32 @@ class DTPipeline:
 
     def build_pipeline(self, df_: pd.DataFrame, drop_missing_threshold=0.2)-> Pipeline:
 
-        df = df_.copy()
 
-        for col, mapping in self.MAPPING.items():
-            df[col] = df[col].map(mapping)
+        cat_pipeline = Pipeline([
+            ('impute_cat', SimpleImputer(strategy='most_frequent')),
+            ('ordinal_encoder', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1))
+        ])
 
-        dp_transformer = DropMissingTransformer(threshold=drop_missing_threshold)
+        num_pipeline = Pipeline([
+            ('impute_num', SimpleImputer(strategy='median'))
+        ])
 
-        Column_transformer = ColumnTransformer(
+        column_transformer = ColumnTransformer(
             transformers=[
-                ("imputer", SimpleImputer(strategy="median"), SELECTED_FEATURES)
+                ('cat', cat_pipeline, make_column_selector(dtype_include=object)),
+                ('num', num_pipeline, make_column_selector(dtype_include=np.number)),
             ], 
-            remainder="passthrough"
+            remainder="drop"
+        )
+
+        model = DecisionTreeClassifier(
+            random_state=42,
+            class_weight='balanced',
         )
 
         return Pipeline([
-            ('drop_missing', uf.DropMissingTransformer(threshold=0.2)),
-            ('column_transformer', Column_transformer),
-            ('selectkbest', SelectKBest(score_func=chi2, k=10)),
-            ('dt', DecisionTreeClassifier(random_state=42))
+            ('drop_missing', uf.DropMissingTransformer(threshold=drop_missing_threshold)),
+            ('column_transformer', column_transformer),
+            ('selectkbest', SelectKBest(score_func=chi2)),
+            ('dt', model)
         ])
-    
-    
-    
-
-        
-
-
-
